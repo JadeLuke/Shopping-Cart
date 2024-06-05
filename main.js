0505d27f4626a348b8dd1f782e1ce41a86f67fb4
JadeLuke <jadeluke.booysen@gmail.com>
2024-06-05 16:30:01 +0200

    started with javascript

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
-let firstMark = Number;
-let secondMark = Number;
-document.getElementById("calcAvg").onclick = function(){
-    firstMark = document.getElementById("mathMark").value;
-    secondMark = document.getElementById("geoMark").value;
+// let firstMark = Number;
+// let secondMark = Number;
+// let avg = (firstMark + secondMark)/2
 
-    alert('Average is ' + `${(firstMark*secondMark)/2}`);
-}
+// avg = document.getElementById("calcAvg").onclick = function avgFn (){
+//     avg = document.getElementById("mathMark").value;
+//     // secondMark = document.getElementById("geoMark").value;
+
+//     alert('Average is ' + `${avg}`);
+// }
+
+let firstMark = document.getElementById("mathMark").value
