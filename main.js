8c92ebd452a4deebfefc3af14fb2efa2d7eb6cb8
JadeLuke <jadeluke.booysen@gmail.com>
2024-06-06 10:42:06 +0200

    added onclick to button

--- main.js
+++ main.js
@@ -2,19 +2,29 @@
 // let secondMark;
 // let thirdMark; 
 // let avg = firstMark*secondMark;
-document.getElementById("calcAvg").onclick = function avgFn(){
-    markMath = Number(document.getElementById("mathMark").value);
-    markGeo = Number(document.getElementById("geoMark").value);
-    markEng = Number(document.getElementById("engMark").value);
-    markAfr = Number(document.getElementById("afrMark").value);
-    markBusiness = Number(document.getElementById("businessMark").value);
-    markIt = Number(document.getElementById("itMark").value);
-    markScience = Number(document.getElementById("scienceMark").value);
-    let avg = (markMath + markGeo + markEng + markAfr + markBusiness + markIt + markScience)/7 ; 
+
+// document.getElementById("calcAvg").onclick = 
+    function avgFn(){
+        markGeo = Number(document.getElementById("geoMark").value);
+        markEng = Number(document.getElementById("engMark").value);
+        markAfr = Number(document.getElementById("afrMark").value);
+        markBusiness = Number(document.getElementById("businessMark").value);
+        markIt = Number(document.getElementById("itMark").value);
+        markScience = Number(document.getElementById("scienceMark").value);
+        let avg = (markMath + markGeo + markEng + markAfr + markBusiness + markIt + markScience)/7; 
+        markMath = Number(document.getElementById("mathMark").value);
+
+
     let results = document.getElementById("displayy");
+   
     if ( avg > 5)
     {
-        results.innerHTML = "Pass"
+        results.innerHTML = "Pass " + (avg).toFixed(1)
+
+
+    }
+    else {
+        results.innerHTML = "fail"
     }
 
 
