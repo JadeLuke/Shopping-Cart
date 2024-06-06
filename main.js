9cbee6a196b26183825133940f9b75ed15302793
JadeLuke <jadeluke.booysen@gmail.com>
2024-06-06 12:32:52 +0200

    finished javascript code and started styling

--- main.js
+++ main.js
@@ -5,26 +5,26 @@
 
 // document.getElementById("calcAvg").onclick = 
     function avgFn(){
-        markGeo = Number(document.getElementById("geoMark").value);
-        markEng = Number(document.getElementById("engMark").value);
-        markAfr = Number(document.getElementById("afrMark").value);
-        markBusiness = Number(document.getElementById("businessMark").value);
-        markIt = Number(document.getElementById("itMark").value);
-        markScience = Number(document.getElementById("scienceMark").value);
-        let avg = (markMath + markGeo + markEng + markAfr + markBusiness + markIt + markScience)/7; 
-        markMath = Number(document.getElementById("mathMark").value);
+       let markMath = Number(document.getElementById("mathMark").value);
+       let  markGeo = Number(document.getElementById("geoMark").value);
+       let markEng = Number(document.getElementById("engMark").value);
+       let   markAfr = Number(document.getElementById("afrMark").value);
+       let  markBusiness = Number(document.getElementById("businessMark").value);
+       let  markIt = Number(document.getElementById("itMark").value);
+       let markScience = Number(document.getElementById("scienceMark").value);
+       let avg = (markMath + markGeo + markEng + markAfr + markBusiness + markIt + markScience)/7; 
 
 
-    let results = document.getElementById("displayy");
+        let results = document.getElementById("displayy");
    
-    if ( avg > 5)
+    if ( avg >= 50)
     {
-        results.innerHTML = "Pass " + (avg).toFixed(1)
+        results.innerHTML = "You have passed with an Average of " + (avg).toFixed(1) + '%'
 
 
     }
     else {
-        results.innerHTML = "fail"
+        results.innerHTML = "You did not pass, your Average is " + (avg).toFixed(1) + '%'
     }
 
 
