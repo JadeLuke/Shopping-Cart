dd2f4f6dbd0fde61d4c161f9a8682828855f8b76
JadeLuke <jadeluke.booysen@gmail.com>
2024-06-10 08:21:21 +0200

    started second example

--- main.js
+++ main.js
@@ -1,24 +1,6 @@
-// function tellFortune () {
+function calculateAgefn (birthYear, currentYear) {
 
-//     let numChildren = prompt('Enter how many kids you want');
-//     let partnerName = prompt('Enter partner name');
-//     let location = prompt('Enter where you want to live');
-//     let jobTitle = prompt('Enter dream job') 
-//     let results = document.getElementById('display')
-//     results.innerHTML = 'You will be a ' + jobTitle + ' in ' + location + ', and married to ' + partnerName + ' with ' + numChildren + ' kids' 
-
-// }
-
-function renderResults() {
-    tellFortune("2", "Sally", "JHB", "Developer");
-    tellFortune("3", "Rihanna", "PTA", "Chef");
-    tellFortune("1", "Susan", "DBN", "Teacher");
-}
-
-
-function tellFortune( noChildren, partersName, location, jobRole) {
-    // let results = document.getElementById('display');
-    console.log('You will be a ' + jobRole + ' in ' + location + ', and married to ' + partersName + ' with ' + noChildren + ' kids')
-return tellFortune
-
+    // let birthYear = Number;
+    // let currentYear = Number;
+    console.log('You are either' + (birthYear-currentYear) + ' or ' + (currentYear - birthYear))
 }
