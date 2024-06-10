422538928c5caad7ea0809ac951153077515c8dc
JadeLuke <jadeluke.booysen@gmail.com>
2024-06-10 08:25:36 +0200

    added functions

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
-function calculateAgefn (birthYear, currentYear) {
+function calculateAge (birthYear, currentYear) {
 
     // let birthYear = Number;
     // let currentYear = Number;
     console.log('You are either' + (birthYear-currentYear) + ' or ' + (currentYear - birthYear))
+}
+
+function ageResults () {
+    calculateAge("2000", "2024");
+    calculateAge("2024", "2000");
+
 }
